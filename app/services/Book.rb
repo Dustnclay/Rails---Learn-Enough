c1e231268c61abd9fd31f6003a88355fc795c5d3
Dustin <DustinTiner@Dustins-Mac-Air.home>
2021-12-31 17:08:19 -0800

    buttons styling added

--- app/services/Book.rb
+++ app/services/Book.rb
@@ -1,7 +1,7 @@
 module Book
     class Search
       def self.by_book(book)
-        Faraday.get 'http://openlibrary.org/search.json?title=the+lord+of+the+rings'
+        Faraday.get 'http://openlibrary.org/search.json?title='+book
       end
     end
   end
