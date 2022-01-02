83cda47550645ef4bf5f094512c954947b4c1a7b
Dustin <DustinTiner@Dustins-Mac-Air.home>
2022-01-02 11:02:11 -0800

    users controller added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,10 +19,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | Bookkeeper"
   end
 
-  test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | Bookkeeper"
-  end
-
 end
